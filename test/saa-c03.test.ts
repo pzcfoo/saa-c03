a16e8e1f4b8aae793195481e86ef29617a312d92
Paul Foo <paul.foo@deswik.com>
2023-02-20 23:33:39 +1000

    Initial commit

---
+++ test/saa-c03.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SaaC03 from '../lib/saa-c03-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/saa-c03-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SaaC03.SaaC03Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
