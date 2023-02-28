1534e529068955e3cd8b1f8551c3b846e2e7fa06
Paul Foo <paul.foo@deswik.com>
2023-02-28 22:51:00 +1000

    improve

--- bin/saa-c03.ts
+++ bin/saa-c03.ts
@@ -2,6 +2,7 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { SaaC03Stack } from '../lib/saa-c03-stack';
+import { ApiCustomDnsStack } from '../lib/rest-api-custom-dns-stack'
 
 const app = new cdk.App();
 new SaaC03Stack(app, 'SaaC03Stack', {
@@ -11,11 +12,15 @@ new SaaC03Stack(app, 'SaaC03Stack', {
 
     /* Uncomment the next line to specialize this stack for the AWS Account
      * and Region that are implied by the current CLI configuration. */
-    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
 
     /* Uncomment the next line if you know exactly what Account and Region you
      * want to deploy the stack to. */
     // env: { account: '123456789012', region: 'us-east-1' },
-
     /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+});
+
+
+new ApiCustomDnsStack(app, 'TestApiStack', {
+    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
 });
--- lib/saa-c03-stack.ts
+++ lib/saa-c03-stack.ts
@@ -13,52 +13,62 @@ export class SaaC03Stack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
-
         const catPicsBucket = new s3.Bucket(
-            this, 'cat-pics-bucket', {
-            removalPolicy: RemovalPolicy.DESTROY,
-            publicReadAccess: false,
-        }
+            this, 'cat-pics-bucket',
+            {
+                removalPolicy: RemovalPolicy.DESTROY,
+                publicReadAccess: false,
+            }
         )
         const animalPicsBucket = new s3.Bucket(
-            this, 'animal-pics-bucket', {
-            removalPolicy: RemovalPolicy.DESTROY,
-            publicReadAccess: false,
-        }
+            this, 'animal-pics-bucket',
+            {
+                removalPolicy: RemovalPolicy.DESTROY,
+                publicReadAccess: false,
+            }
+        )
+        const userPolicy = new iam.ManagedPolicy(
+            this, "user-policy",
+            {
+                statements: [
+                    new iam.PolicyStatement(
+                        {
+                            sid: "AllowS3",
+                            actions: ["s3:*"],
+                            resources: ["arn:aws:s3:::*"],
+                            effect: iam.Effect.ALLOW,
+                        }
+                    ),
+                    new iam.PolicyStatement(
+                        {
+                            sid: "DenyCatPicsS3",
+                            actions: ["s3:*"],
+                            resources: [catPicsBucket.bucketArn],
+                            effect: iam.Effect.DENY,
+                        }
+                    )
+                ]
+            }
         )
 
-        const userPolicy = new iam.ManagedPolicy(
-            this, "user-policy", {
-            statements: [
-                new iam.PolicyStatement(
-                    {
-                        sid: "AllowS3",
-                        actions: ["s3:*"],
-                        resources: ["arn:aws:s3:::*"],
-                        effect: iam.Effect.ALLOW,
-                    }
-                ),
-                new iam.PolicyStatement(
-                    {
-                        sid: "DenyCatPicsS3",
-                        actions: ["s3:*"],
-                        resources: [catPicsBucket.bucketArn],
-                        effect: iam.Effect.DENY,
-                    }
-                )
-            ]
-        }
+        const devGroup = new iam.Group(
+            this, "dev-group",
+            {
+                groupName: "developers",
+                managedPolicies: [
+                    iam.ManagedPolicy.fromAwsManagedPolicyName("IAMUserChangePassword"),
+                    userPolicy,
+                ]
+            }
         )
 
         const sally = new iam.User(
-            this, 'sally', {
-            password: SecretValue.unsafePlainText("Harryp0tter!"),
-            passwordResetRequired: true,
-            managedPolicies: [
-                iam.ManagedPolicy.fromAwsManagedPolicyName("IAMUserChangePassword"),
-                userPolicy,
-            ]
-        }
+            this, 'sally',
+            {
+                password: SecretValue.unsafePlainText("Harryp0tter!"),
+                passwordResetRequired: true,
+                groups: [devGroup]
+            }
         )
     }
 }
