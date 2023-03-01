af8121edb94a207dddaff50dfd2a40d386b93461
Paul Foo <paul.foo@polymathian.com>
2023-03-01 15:35:08 +1000

    finish example

--- lib/rest-api-custom-dns-stack.ts
+++ lib/rest-api-custom-dns-stack.ts
@@ -4,6 +4,9 @@ import {
     StackProps,
     Stack,
     CfnOutput,
+    aws_route53_targets as targets,
+    aws_route53 as r53,
+    aws_certificatemanager as acm,
     aws_lambda as lambda,
     aws_apigateway as apigateway,
 } from 'aws-cdk-lib'
@@ -13,11 +16,17 @@ export class ApiCustomDnsStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
+        // reflet the existing certificate
+        const domainCert = acm.Certificate.fromCertificateArn(
+            this,
+            'cert', "arn:aws:acm:us-east-1:xxxxxxxxxx:certificate/aaaaaaaaa-bbbb-bbbb-ccccccccc"
+        )
         const api = new apigateway.LambdaRestApi(this, 'test-api', {
-            domainName: new apigateway.DomainName(this, 'custom-domain-name', {
+            domainName: {
                 domainName: "test.yoloswag.org",
-                certificate: null,
-            }),
+                certificate: domainCert,
+            },
+            endpointTypes: [apigateway.EndpointType.REGIONAL],
             handler: new lambda.Function(this, 'hello-world-lambda', {
                 handler: 'index.handler',
                 code: lambda.Code.fromInline(
@@ -31,6 +40,14 @@ export class ApiCustomDnsStack extends Stack {
                 runtime: lambda.Runtime.NODEJS_16_X,
             })
         });
+
+
+        const hostedZone = r53.HostedZone.fromLookup(this, 'test-hosted-zone', { domainName: 'yoloswag.org' });
+        const record = new r53.ARecord(this, 'test-a-record', {
+            zone: hostedZone,
+            recordName: "test.yoloswag.org",
+            target: r53.RecordTarget.fromAlias(new targets.ApiGateway(api)),
+        })
         new CfnOutput(this, "api-url", { value: api.url })
     }
 }
