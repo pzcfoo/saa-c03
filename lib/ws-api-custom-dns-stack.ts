6691e8952725cb9ce46f6d3a250d4692d033c451
Paul Foo <paul.foo@deswik.com>
2023-03-02 11:38:45 +1000

    customdns for websocket api

--- lib/ws-api-custom-dns-stack.ts
+++ lib/ws-api-custom-dns-stack.ts
@@ -11,23 +11,19 @@ import {
     aws_certificatemanager as acm,
     aws_lambda as lambda,
     aws_apigateway as apigateway,
+    aws_apigatewayv2 as apigwv2,
 } from 'aws-cdk-lib'
-import { WebSocketApi, WebSocketStage } from "@aws-cdk/aws-apigatewayv2-alpha";
+import { WebSocketApi, WebSocketStage, DomainName, ApiMapping } from "@aws-cdk/aws-apigatewayv2-alpha";
 import { WebSocketLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
+import { ApiGatewayv2DomainProperties } from 'aws-cdk-lib/aws-route53-targets';
 
 
 export class WsApiCustomDnsStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
-        const hostedZone = r53.HostedZone.fromLookup(this, 'test-hosted-zone', { domainName: 'yoloswag.org' });
 
-        // create certificate
-        //const cert = new acm.Certificate(this, 'test-cert', {
-        //    domainName: 'test.yoloswag.org',
-        //    validation: acm.CertificateValidation.fromDns(hostedZone),
-        //});
-
+        // create lambdas backends for ws api
         const connectHandler = new lambda.Function(this, 'connect-handler', {
             runtime: lambda.Runtime.PYTHON_3_9,
             handler: 'function.connect_handler',
@@ -46,6 +42,7 @@ export class WsApiCustomDnsStack extends Stack {
             code: lambda.Code.fromAsset(path.join("lib", "lambdas"))
         });
 
+        // create ws api
         const websocketApi = new WebSocketApi(this, "test-ws-api-gateway", {
             connectRouteOptions: {
                 integration: new WebSocketLambdaIntegration("connect-integration", connectHandler),
@@ -57,21 +54,46 @@ export class WsApiCustomDnsStack extends Stack {
         websocketApi.addRoute("message", {
             integration: new WebSocketLambdaIntegration("onmessage-handler", onMessageHandler),
         });
+
+        const hostedZone = r53.HostedZone.fromLookup(this, 'test-hosted-zone', { domainName: 'yoloswag.org' });
+        const customDomainName = `ws.${hostedZone.zoneName}`
+        const cert = new acm.Certificate(this, 'test-cert', {
+            domainName: customDomainName,
+            validation: acm.CertificateValidation.fromDns(hostedZone),
+        });
+        const wsDomain = new DomainName(this, 'domain-name', {
+            domainName: customDomainName,
+            certificate: cert
+        })
+        // Dns record for the new domain name
+        new r53.ARecord(this, 'test-a-record', {
+            zone: hostedZone,
+            recordName: customDomainName,
+            target: r53.RecordTarget.fromAlias(
+                new ApiGatewayv2DomainProperties(
+                    wsDomain.regionalDomainName,
+                    wsDomain.regionalHostedZoneId,
+                )
+            )
+        });
+
+        // the deploy stage for the api
+        // The custon domman name mapping is also applied here.
         const deployStage = new WebSocketStage(this, 'test-ws-stage', {
             stageName: "prod",
             autoDeploy: true,
             webSocketApi: websocketApi,
+            domainMapping: {
+                domainName: wsDomain
+            }
         })
+
+        // allow message handler to manage connections for 'posting' back to connections
         onMessageHandler.addToRolePolicy(new iam.PolicyStatement({
             effect: iam.Effect.ALLOW,
             actions: ["execute-api:ManageConnections"],
             resources: [`arn:aws:execute-api:${this.region}:${this.account}:${websocketApi.apiId}/${deployStage.stageName}/*`],
         }));
-        //new r53.ARecord(this, 'test-a-record', {
-        //    zone: hostedZone,
-        //    recordName: "test.yoloswag.org",
-        //    target: r53.RecordTarget.fromAlias(new targets.ApiGateway(api)),
-        //})
         new CfnOutput(this, "api-url", { value: websocketApi.apiEndpoint });
     }
 }
